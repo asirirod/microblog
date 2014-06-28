71447aa849fe768c7867055e0f9118e9d37aa82e
Asiri Rodrigo <joseph.a.rodrigo@gmail.com>
2014-06-28 14:09:02 -0500

    Part VII Complete

--- config.py
+++ config.py
@@ -4,6 +4,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+# mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['hotrodx@gmail.com']
+
 OPENID_PROVIDERS = [
     { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
     { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
